03fca8271f7d0c8ae94d48185be9171771097e5c
chiredan <neonik93@gmail.com>
2013-09-26 17:39:35 +0200

    Alpha1 release

--- woeDice.js
+++ woeDice.js
@@ -1,6 +1,20 @@
+$("#box").ready(buildDice);
+//Variables Block
 
+// Functions Block
 
+function buildDice() {
+    buildDiceDiv(document.getElementById("messageFormBox").getElementsByTagName("img").item(0).alt);
+}
 
+function buildDiceDiv(user) {
+    var userInput = '<input id="diceUser" value="' + user + '"/>';
+    var opponentInput = '<input id="diceOpponent" value="tu wpisz nazwę gracza"/>'
+    var button = '<button id="roll" onclick="rollDice()">losuj</button>';
+    //var script = '<script src="https://raw.github.com/chiredan/WorldOfEquestriaDice/MainBranch/woeDice.js"> </script>
+    $("#box").before('<div id="dice">' + userInput + opponentInput + button+ '</div>');
+
+}
 
 // Action Listner
 
