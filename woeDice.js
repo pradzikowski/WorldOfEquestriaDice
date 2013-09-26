19682a8df033a37367753fe3d1720b910035b50d
chiredan <neonik93@gmail.com>
2013-09-26 14:37:47 +0200

    test commit I

--- woeDice.js
+++ woeDice.js
@@ -3,22 +3,7 @@
 
 
 // Action Listner
-$("#box").ready(buildDice);
-//Variables Block
 
-// Functions Block
-
-function buildDice() {
-    buildDiceDiv(document.getElementById("messageFormBox").getElementsByTagName("img").item(0).alt);
-}
-
-function buildDiceDiv(user) {
-    var userInput = '<input id="diceUser" value="' + user + '"/>';
-    var opponentInput = '<input id="diceOpponent" value="tu wpisz nazwę gracza"/>'
-    var button = '<button id="roll" onclick="rollDice()">losuj</button>';
-    $("#box").before('<div id="dice">' + userInput + opponentInput + button + '</div>');
-
-}
 
 function rollDice() {
 
