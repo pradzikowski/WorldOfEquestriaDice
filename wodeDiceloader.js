52b528885a81ea70967b0f0bfa4e70559821d9df
chiredan <neonik93@gmail.com>
2013-09-29 17:44:07 +0200

    Alpha2 pre2a
    next DiceLoader change

--- wodeDiceloader.js
+++ wodeDiceloader.js
@@ -3,21 +3,28 @@
 // @namespace   WoEDiceLoader
 // @include     http://worldofequestria.pl/*
 // @version     2
+// @downloadURL https://raw.github.com/chiredan/WorldOfEquestriaDice/MainBranch/wodeDiceloader.js
 // @updateURL https://raw.github.com/chiredan/WorldOfEquestriaDice/MainBranch/wodeDiceloader.js
 // @author Chiredan&Vienes
 // ==/UserScript==
 
+// var block
+var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement;
 
 // Add woeDiceScript
     (function(){
-        if (typeof unsafeWindow.woeDiceScript == 'undefined') {
-            var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
-                GM_JQ = document.createElement('script');
+        loadLibrary("woeDice");
+            
+    });
+
+
+    function loadLibrary(libName){
+        var  GM_JQ = document.createElement('script');
     
-            GM_JQ.src = 'https://raw.github.com/chiredan/WorldOfEquestriaDice/MainBranch/woeDice.js';
+            GM_JQ.src = 'https://raw.github.com/chiredan/WorldOfEquestriaDice/MainBranch/'+libName+'.js';
             GM_JQ.type = 'text/javascript';
             GM_JQ.async = true;
     
             GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
-        }
-    })();
+    }
+// Simple woeLoaderFunctions
