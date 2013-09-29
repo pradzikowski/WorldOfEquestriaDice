ee52a3cd3d394f36542977640aa7b4ffa14d717e
chiredan <neonik93@gmail.com>
2013-09-29 18:12:55 +0200

    Update wodeDiceloader.js

--- wodeDiceloader.js
+++ wodeDiceloader.js
@@ -1,23 +1,32 @@
-// ==UserScript==
-// @name        WoEChatDiceLoader
 // @namespace   WoEDiceLoader
 // @include     http://worldofequestria.pl/*
-// @version     3
+// @version     3a
+// @downloadURL https://raw.github.com/chiredan/WorldOfEquestriaDice/MainBranch/wodeDiceloader.js
+// @version     3a
 // @updateURL https://raw.github.com/chiredan/WorldOfEquestriaDice/MainBranch/wodeDiceloader.js
 // @author Chiredan&Vienes
 // ==/UserScript==
 
+// var block
+var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement;
 
 // Add woeDiceScript
-    (function(){
-        if (typeof unsafeWindow.woeDiceScript == 'undefined') {
-            var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
-                GM_JQ = document.createElement('script');
-    
-            GM_JQ.src = 'https://raw.github.com/chiredan/WorldOfEquestriaDice/MainBranch/woeDice.js';
-            GM_JQ.type = 'text/javascript';
-            GM_JQ.async = true;
-    
-            GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
-        }
-    })();
+(function() {
+
+    loadLibrary("woeDice");
+
+});
+
+
+// Simple woeLoaderFunctions
+
+function loadLibrary(libName) {
+    var GM_JQ = document.createElement('script');
+
+    GM_JQ.src = 'https://raw.github.com/chiredan/WorldOfEquestriaDice/MainBranch/woeDice.js';
+    GM_JQ.src = 'https://raw.github.com/chiredan/WorldOfEquestriaDice/MainBranch/' + libName + '.js';
+    GM_JQ.type = 'text/javascript';
+    GM_JQ.async = true;
+
+    GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
+}
